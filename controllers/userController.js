e5740a6e3b003932ae6cee8c500e56956f72cc15
sandhyakyanam <sandhyakyanam77@gmail.com>
2025-09-14 23:33:19 +0530

    Worked on the curd operations

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,22 +4,14 @@ async function getuserInfo(req,res)
 {
     try{
         const userResult = await userModel.getUserDetails();
-        res.render('userInfo',{userInfo:userResult});
-    }catch(err)
-    {
-        res.status(500).send('Server error');
-    }
-}
-async function signUp(req,res) {
-    try{
-       res.render('signUp');
+        res.json(userResult);
     }catch(err)
     {
         res.status(500).send('Server error');
     }
 }
 
-async function signUpAction(req,res)
+async function signUpUser(req,res)
 {
     try{
         userdata = {
@@ -30,12 +22,90 @@ async function signUpAction(req,res)
             status : req.body.status
         }
         const insertUser = await userModel.insertUser(userdata);
+        if((insertUser.insertId) > 0)
+        {
+          res.status(201).json({
+            success:true,
+            message:"User created successfully"
+          });
+        }
     }
     catch(err)
     {
-        console.error("Error in signUpAction:", err);
+        console.error("Error in signUpUser:", err);
         res.status(500).send('Server error');
     }
 }
 
-module.exports = { getuserInfo , signUp, signUpAction};   
+async function editUser(req,res) {
+    try{
+        
+        var editUserId = req.params.id;
+        if((editUserId) > 0)
+        {
+           userData = await userModel.checkExistuser(editUserId);  
+           if((userData[0].id) > 0)
+           {
+                var editUserData = {
+                    firstname : req.body.firstname,
+                    lastname : req.body.lastname,
+                    email : req.body.email,
+                    password : req.body.password,
+                    status : req.body.status,
+                    id : editUserId
+                }
+                updatedData = await userModel.editUserDetails(editUserData);
+                console.log(updatedData);
+                if(updatedData.affectedRows === 1)
+                {
+                    res.status(200).json({
+                        success:true,
+                        message:"User Updated successfully"
+                    });
+                }
+           }else{
+                res.status(409).json({
+                    success:true,
+                    message : "User Not Exists"
+                });
+           }
+        } 
+    }catch(err){
+        console.log('Something went wrong while editing the user',err);
+        res.status(500).send('Server error');
+    }
+}
+async function deleteUser(req,res)
+{
+    try{
+       var deleteUserId = req.params.id;
+       var deleteUserResult = await userModel.deleteUserById(deleteUserId);
+       if(deleteUserResult.affectedRows === 1)
+       {
+            res.status(200).json({ 
+                status: 200, 
+                message: "User deleted successfully" 
+            });
+       }
+    }
+    catch(err)
+    {
+      console.log("Something went wrong with the delete",err);
+    }
+}
+async function getUserById(req,res)
+{
+   try{
+      
+       var userId = req.params.id;
+       if(userId > 0){
+          var getUserResult = await userModel.getUserDetailById(userId);
+          console.log(getUserResult);
+       } 
+
+   }catch(err)
+   {
+     console.log('Something went wrong while getting the user details',err);
+   }
+}
+module.exports = { getuserInfo ,signUpUser, editUser, deleteUser, getUserById};   
--- models/userModel.js
+++ models/userModel.js
@@ -21,9 +21,62 @@ async function insertUser(userdata) {
       throw err;
     }
   }
-  
 
+async function checkExistuser(id)
+{
+  try {
+    const [rows] = await db.query("SELECT id FROM users WHERE id = ?", [id]);
+    return rows;
+  } catch (err) {
+      throw err;
+  }
+}
+
+async function editUserDetails(editUserDetails)
+{
+  try{
+    const [result] = await db.query(`
+    UPDATE users 
+    SET firstname = ?, lastname = ?, email = ?, password = ?, status = ?
+    WHERE id = ?`, [
+      editUserDetails.firstname,
+      editUserDetails.lastname,
+      editUserDetails.email,
+      editUserDetails.password,
+      editUserDetails.status,
+      editUserDetails.id
+    ]);
+    return result; 
+  }
+  catch(err)
+  {
+    throw err;
+  }
+}
+async function  deleteUserById(id) {
+   try{
+      const [deleteUser] = await db.query(`DELETE from users where id = ?`,[id]);
+      return deleteUser;
+   }
+   catch(err)
+   {
+     throw err;
+   }
+}
+async function getUserDetailById(id)
+{
+  try {
+    const [rows] = await db.query("SELECT * FROM users WHERE id = ?", [id]);
+    return rows;
+  } catch (err) {
+      throw err;
+  }
+}
 module.exports = {
     getUserDetails ,
-    insertUser
+    insertUser,
+    checkExistuser,
+    editUserDetails,
+    deleteUserById,
+    getUserDetailById
 }
