e2b3c0ea478293af2a55099da60d48fd0d53cc2b
sandhyakyanam <sandhyakyanam77@gmail.com>
2025-09-21 22:15:35 +0530

    Worked on the nodemailer and the file uplod

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const db = require('../config/db');
 const userController = require('../controllers/userController');
 const middleware = require('../middleware/middleware');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination : function(req,file,cb)
+    {
+        cb(null,"public/uploads/")
+    },
+    filename : function(req,file,cb)
+    {
+        cb(null,Date.now()+ path.extname(file.originalname));
+    }
+});
+const upload = multer({storage}); 
 
 router.get('/',userController.getuserInfo);
 router.post('/signUp',userController.signUpUser);
@@ -11,6 +25,7 @@ router.delete('/deleteuser/:id',userController.deleteUser);
 router.get('/getUserDetails/:id',userController.getUserById);
 router.get('/login',userController.checkUser);
 router.get('/profile',middleware ,userController.getuserProfile);
-
+router.post('/upload',middleware,upload.single("file"),userController.insertUserFile);
+router.post('/sendMail',userController.sendMail);
 
 module.exports = router;
